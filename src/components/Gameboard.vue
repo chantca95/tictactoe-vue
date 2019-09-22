73c94b70a1edffe21be5af62f03d878fa0877d5f
Alex Chan <alexchan@leandatainc.com>
2019-09-22 00:20:01 -0700

    scaffolding

---
+++ src/components/Gameboard.vue
@@ -0,0 +1,46 @@
+<template>
+  <table id="game-grid">
+    <div class="game-tile"></div>
+    <div class="game-tile"></div>
+    <div class="game-tile"></div>
+    <div class="game-tile"></div>
+    <div class="game-tile"></div>
+    <div class="game-tile"></div>
+    <div class="game-tile"></div>
+    <div class="game-tile"></div>
+    <div class="game-tile"></div>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "Gameboard",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      currentTurn: 0,
+    }
+  }
+};
+</script>
+<style scoped>
+  #game-grid{
+    display: grid;
+    grid-template-columns: 100px 100px 100px;
+    grid-template-rows: 100px 100px 100px;
+    grid-column-gap: 5px;
+    grid-row-gap: 5px;
+    background-color: lemonchiffon;
+    border: 5px lemonchiffon solid;
+  }
+  .game-tile{
+    font-size: 30px;
+    background-color: navy;
+    line-height: 100px;
+    vertical-align: middle;
+    color: white;
+  }
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="home">
+    <img alt="Tic Tac Toe logo" src="../assets/tictactoe.jpeg" />
+    <div class="hello">
+      <h1>{{ msg }}</h1>
+    <div class="column-group">
+      <Gameboard v-if="gameHasStarted" ></Gameboard>
+      <div @mousedown="startGame" v-else id="start-button">Start</div>      
+      <div v-if="gameHasStarted" @mousedown="startGame" id="restart-button">Restart</div>
+
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// you import components that you're going to use in your Home.vue page
+import Gameboard from "@/components/Gameboard.vue";
+
+export default {
+  name: "home",
+  data() {
+    return {
+      msg: "Welcome to Tic Tac Toe",
+      gameHasStarted: false
+    }
+  },
+  methods: {
+    startGame() {
+      this.gameHasStarted = true;
+    }
+  },
+  components: {
+    Gameboard
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+#start-button, #restart-button {
+  font-size: 20px;
+  width: 100px;
+  padding: 10px;
+  margin: 20px 0px;
+  text-align: middle;
+  background-color: navy;
+  color: white;
+}
+.column-group {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
