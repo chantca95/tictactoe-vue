1ada652983f5f3b573526e37daea498e00e26ccd
Alex Chan <alexchan@leandatainc.com>
2019-09-22 02:32:36 -0700

    tiles responsive

---
+++ src/components/GameTile.vue
@@ -0,0 +1,20 @@
+<template>
+  <td v-on:click="tileClicked" class="game-tile">{{ value === "0" ? '' : value }}</td>
+</template>
+
+<script>
+export default {
+  name: "Gameboard",
+  props: {
+    row: Number,
+    col: Number,
+    value: String,
+  },
+  methods: {
+    tileClicked() {
+      console.log('tile response: ' + this.row + ' ' + this.col)
+      this.$emit('tile-clicked', this.row, this.col,);
+    }
+  }
+}
+</script>
--- src/components/Gameboard.vue
+++ src/components/Gameboard.vue
@@ -1,18 +1,22 @@
 <template>
   <table id="game-grid">
-    <div class="game-tile"></div>
-    <div class="game-tile"></div>
-    <div class="game-tile"></div>
-    <div class="game-tile"></div>
-    <div class="game-tile"></div>
-    <div class="game-tile"></div>
-    <div class="game-tile"></div>
-    <div class="game-tile"></div>
-    <div class="game-tile"></div>
+    <tr v-for="row in 3" :key="row - 1">
+      <GameTile 
+        v-for="col in 3"
+        :row="row - 1" 
+        :col="col - 1"
+        :value="tiles[row-1][col-1]" 
+        :key="col - 1" 
+        @tile-clicked="recordTileClick">
+      </GameTile>
+    </tr>
   </table>
 </template>
 
 <script>
+
+import GameTile from "@/components/GameTile.vue";
+
 export default {
   name: "Gameboard",
   props: {
@@ -20,25 +24,43 @@ export default {
   },
   data() {
     return {
-      currentTurn: 0,
+      currentTurn: 1,
+      // initialized with 0s. The GameTile Component will read 0s as null values and won't display anything
+      tiles: [
+        ['0','0','0'],
+        ['0','0','0'],
+        ['0','0','0'],
+      ]
     }
+  },
+  methods: {
+    recordTileClick(row, col){
+      console.log('row is: ' + row);
+      console.log('col is: ' + col);
+    }
+  },
+  components: {
+    GameTile
   }
 };
 </script>
-<style scoped>
+<style>
   #game-grid{
-    display: grid;
+    height: 350px;
+    width: 350px;
+    /* display: grid;
     grid-template-columns: 100px 100px 100px;
     grid-template-rows: 100px 100px 100px;
     grid-column-gap: 5px;
-    grid-row-gap: 5px;
+    grid-row-gap: 5px;*/
     background-color: lemonchiffon;
-    border: 5px lemonchiffon solid;
+    border: 5px gold solid; 
   }
   .game-tile{
     font-size: 30px;
     background-color: navy;
-    line-height: 100px;
+    height: 100px;
+    width: 100px;
     vertical-align: middle;
     color: white;
   }
